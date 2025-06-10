329e0a47b90188f16f5cbf9e5c35a730138d29c0
İlayda Bakırcıoğlu <119436349+ilaydabkrc@users.noreply.github.com>
2025-06-10 21:00:32 +0300

    List 1 and List 2

---
+++ List1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+
+        List<string> davetliler = new List<string>()
+        {
+            "Bülent Ersoy",
+            "Ajda Pekkan",
+            "Ebru Gündeş",
+            "Hadise",
+            "Hande Yener",
+            "Tarkan",
+            "Funda Arar",
+            "Demet Akalın"
+
+
+        };
+        Console.WriteLine("** Davetliler **\n");
+
+        int sıra = 1;
+        foreach (string isim in davetliler)
+        {
+            Console.WriteLine($"{sıra}-{isim}");
+            sıra++;
+        }
+    }
+
+}
---
+++ List2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class program
+{
+    static void Main()
+    {
+        List<string> kahveListesi = new List<string>();
+
+        for(int i = 1; i <= 5; i++)
+        {
+
+            Console.WriteLine($"Kahve {i} adını girin: ");
+            string kahve = Console.ReadLine();
+
+            kahveListesi.Add(kahve);
+        }
+        Console.WriteLine("Girilen kahve isimleri: ");
+
+        int sıra = 1;
+        foreach(string kahve in kahveListesi)
+        {
+            Console.WriteLine(kahve);
+            
+            sıra++;
+        }
+
+    }
+
+
+
+}
